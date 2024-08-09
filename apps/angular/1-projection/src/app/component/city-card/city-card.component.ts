307c28e47e061225728677581c89dce5bc81ce2e
Kay Rechsteiner <k.rechsteiner@betur.ch>
2024-08-09 15:58:21 +0200

    feat(Answer:1): challenge solution

--- apps/angular/1-projection/src/app/component/city-card/city-card.component.ts
+++ apps/angular/1-projection/src/app/component/city-card/city-card.component.ts
@@ -1,13 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { CityStore } from '../../data-access/city.store';
+import {
+  FakeHttpService,
+  randomCity,
+} from '../../data-access/fake-http.service';
+import { CardRowDirective } from '../../ui/card/card-row.directive';
+import { CardComponent } from '../../ui/card/card.component';
+import { ListItemComponent } from '../../ui/list-item/list-item.component';
 
 @Component({
   selector: 'app-city-card',
-  template: 'TODO City',
+  template: `
+    <app-card [items]="cities()" class="bg-light-yellow" (onAdd)="addCity()">
+      <img src="assets/img/city.png" width="200px" />
+      <ng-template cardRow="cities()" let-city>
+        <app-list-item (onDelete)="deleteCity(city.id)">
+          {{ city.name }}
+        </app-list-item>
+      </ng-template>
+    </app-card>
+  `,
+  styles: [
+    `
+      .bg-light-yellow {
+        background-color: rgba(250, 250, 0, 0.1);
+      }
+    `,
+  ],
   standalone: true,
-  imports: [],
+  imports: [CardComponent, ListItemComponent, CardRowDirective],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CityCardComponent implements OnInit {
-  constructor() {}
+export class CityCardComponent {
+  private http = inject(FakeHttpService);
+  private store = inject(CityStore);
 
-  ngOnInit(): void {}
+  cities = this.store.cities;
+
+  constructor() {
+    this.http.fetchCities$.subscribe((c) => this.store.addAll(c));
+  }
+
+  addCity() {
+    this.store.addOne(randomCity());
+  }
+
+  deleteCity(id: number) {
+    this.store.deleteOne(id);
+  }
 }
