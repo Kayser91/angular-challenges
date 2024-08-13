6809e870b61d60468f3b387fe87bc8a58683f4ea
Kay Rechsteiner <k.rechsteiner@betur.ch>
2024-08-13 14:21:35 +0200

    feat(Answer:8): resolve pure pipe challenge

--- apps/angular/8-pure-pipe/src/app/app.component.ts
+++ apps/angular/8-pure-pipe/src/app/app.component.ts
@@ -1,21 +1,18 @@
-import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { ComputePipe } from './compute.pipe';
 
 @Component({
   standalone: true,
-  imports: [NgFor],
+  imports: [ComputePipe],
   selector: 'app-root',
   template: `
-    <div *ngFor="let person of persons; let index = index">
-      {{ heavyComputation(person, index) }}
-    </div>
+    @for (person of persons; track $index) {
+      <div>
+        {{ person | compute: $index }}
+      </div>
+    }
   `,
 })
 export class AppComponent {
-  persons = ['toto', 'jack'];
-
-  heavyComputation(name: string, index: number) {
-    // very heavy computation
-    return `${name} - ${index}`;
-  }
+  persons: string[] = ['toto', 'jack', 'michael', 'james'];
 }
